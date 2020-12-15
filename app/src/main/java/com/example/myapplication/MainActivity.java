c031fa72c0e46deea8f072377fa1ada4bd360ff7
wppfiyy <wpp@qq.com>
2020-12-15 21:34:54 +0800

    第二次改

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+		//来了法地儿~
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 		//哈哈哈哈美女王屁屁
